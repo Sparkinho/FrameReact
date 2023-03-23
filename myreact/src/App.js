0d832ee6a4a7ea65410ef8c0b84f27da1a8cc66c
Sparkinho <113615841+Sparkinho@users.noreply.github.com>
2023-03-22 22:17:02 -0300

    primeiro react

---
+++ myreact/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Teste de pagina</h1>
+
+    </div>
+  );
+}
+
+export default App;
